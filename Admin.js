d73e9082ef13fb37f6eb9f8926ff680b029e706f
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-10 18:12:57 -0300

    done

--- Admin.js
+++ Admin.js
@@ -1,11 +1,10 @@
 import User from "./User.js";
 
 export default class Admin extends User {
-  constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+  constructor(nome, email, nascimento, role = "admin", ativo = true) {
     super(nome, email, nascimento, role, ativo);
   }
-  criarCurso(nomeDoCurso, vagas){
-    return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
+  criarCurso(nomeDoCurso, vagas) {
+    return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`;
   }
 }
-
--- index.js
+++ index.js
@@ -4,4 +4,3 @@ import Docente from "./Docente.js";
 
 const novoUser = new User("Mario", "m@m.com", "2021-01-01");
 console.log(novoUser.exibirInfos());
-
